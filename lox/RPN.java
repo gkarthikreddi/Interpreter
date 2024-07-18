d0d2ee5ef855fc46764607318fd17dd848bad40b
Karthik <paulatreides@duck.com>
2024-07-18 06:47:07 +0530

    RPN challenge

---
+++ lox/RPN.java
@@ -0,0 +1,40 @@
+package craftinginterpreter.lox;
+
+class RPN implements Expr.Visitor<String> {
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return expr.left.accept(this) + expr.right.accept(this) + expr.operator.lexeme ;
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        // Apparently in RPN we don't need explicit grouping
+        return expr.expression.accept(this);
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return "(-" + expr.right.accept(this) + ")";
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.value == null) return "nil";
+        return expr.value.toString();
+    }
+
+    public static void main(String[] args) {
+        Expr expression  = new Expr.Binary(
+                new Expr.Unary(
+                    new Token(TokenType.MINUS, "-", null, 1),
+                    new Expr.Literal(123)),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Grouping(
+                    new Expr.Literal(45.67)));
+        System.out.println(new RPN().print(expression));
+    }
+}
